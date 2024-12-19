a9a7a8d9c91420a2613ecc04071d8b28f8998087
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-19 19:05:53 +0600

    first commit

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+// import { StudentRoutes } from './app/modules/student/student.route';
+
+const app: Application = express();
+
+// parser
+app.use(express.json());
+app.use(cors());
+
+// /api/v1/students/create-student
+
+// application routes
+// app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  //   res.send('Hello World!')
+  const a = 10;
+
+  res.send(a);
+};
+
+app.get('/', getAController);
+
+// console.log(process.cwd()); // E:\web\Programming Hero\Level 2\Mission 01-Be A Typescript Technocrat\Module 8-Mastering The Core concept of Mongoose\first-project
+
+export default app;

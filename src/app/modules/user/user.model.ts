05e7b84359b983492328196253fe8bf485cccb84
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-19 20:12:54 +0600

    user.model created

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,2 +1,62 @@
+import { Schema, model } from 'mongoose';
+import { TUser } from './user.interface';
+
+const userSchema = new Schema<TUser>(
+    {
+        id: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        needsPasswordChange: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        passwordChangedAt: {
+            type: Date,
+            default: null,
+        },
+        role: {
+            type: String,
+            enum: ['admin', 'user'],
+            required: true,
+        },
+        status: {
+            type: String,
+            enum: ['active', 'blocked'],
+            default: 'active',
+        },
+        isBlocked: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        isDeleted: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
 
 
+export const UserModel = model<TUser>('User', userSchema);

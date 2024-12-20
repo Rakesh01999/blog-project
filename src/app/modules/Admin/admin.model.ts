ca4c5103b9e7c7f02526e131b7d6d8b2eeed5fac
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-20 13:09:21 +0600

    admin.model  -modified

--- src/app/modules/Admin/admin.model.ts
+++ src/app/modules/Admin/admin.model.ts
@@ -1,129 +1,58 @@
-
-
 import { Schema, model } from 'mongoose';
-import { BloodGroup, Gender } from './admin.constant';
-import { AdminModel, TAdmin, TUserName } from './admin.interface';
-
-const userNameSchema = new Schema<TUserName>({
-  firstName: {
-    type: String,
-    required: [true, 'First Name is required'],
-    trim: true,
-    maxlength: [20, 'Name can not be more than 20 characters'],
-  },
-  middleName: {
-    type: String,
-    trim: true,
-  },
-  lastName: {
-    type: String,
-    trim: true,
-    required: [true, 'Last Name is required'],
-    maxlength: [20, 'Name can not be more than 20 characters'],
-  },
-});
+import { AdminModel, TAdmin } from './admin.interface';
 
 const adminSchema = new Schema<TAdmin, AdminModel>(
   {
     id: {
       type: String,
-      required: [true, 'ID is required'],
+      required: [true, 'Admin ID is required'],
       unique: true,
     },
-    user: {
-      type: Schema.Types.ObjectId,
-      required: [true, 'User id is required'],
-      unique: true,
-      ref: 'User',
-    },
-    designation: {
-      type: String,
-      required: [true, 'Designation is required'],
-    },
-    name: {
-      type: userNameSchema,
-      required: [true, 'Name is required'],
-    },
-    gender: {
-      type: String,
-      enum: {
-        values: Gender,
-        message: '{VALUE} is not a valid gender',
-      },
-      required: [true, 'Gender is required'],
-    },
-    dateOfBirth: { type: Date },
     email: {
       type: String,
       required: [true, 'Email is required'],
       unique: true,
+      trim: true,
+      lowercase: true,
     },
-    contactNo: { type: String, required: [true, 'Contact number is required'] },
-    emergencyContactNo: {
+    password: {
       type: String,
-      required: [true, 'Emergency contact number is required'],
+      required: [true, 'Password is required'],
     },
-    bloogGroup: {
+    role: {
       type: String,
-      enum: {
-        values: BloodGroup,
-        message: '{VALUE} is not a valid blood group',
-      },
+      enum: ['admin'], // Admin role only
+      default: 'admin',
+      required: [true, 'Role is required'],
     },
-    presentAddress: {
-      type: String,
-      required: [true, 'Present address is required'],
-    },
-    permanentAddress: {
-      type: String,
-      required: [true, 'Permanent address is required'],
-    },
-    profileImg: { type: String },
-    isDeleted: {
+    isBlocked: {
       type: Boolean,
       default: false,
     },
   },
   {
+    timestamps: true, // Automatically add createdAt and updatedAt fields
     toJSON: {
       virtuals: true,
     },
-  },
+  }
 );
 
-// generating full name
-adminSchema.virtual('fullName').get(function () {
-  return (
-    this?.name?.firstName +
-    '' +
-    this?.name?.middleName +
-    '' +
-    this?.name?.lastName
-  );
-});
-
-// filter out deleted documents
+// Pre-hook: Filter out blocked admins if necessary
 adminSchema.pre('find', function (next) {
-  this.find({ isDeleted: { $ne: true } });
+  this.find({ isBlocked: { $ne: true } });
   next();
 });
 
-adminSchema.pre('findOne', function (next) {
-  this.find({ isDeleted: { $ne: true } });
-  next();
-});
+// Static method: Check if an admin exists by email
+adminSchema.statics.isAdminExists = async function (email: string) {
+  return await Admin.findOne({ email });
+};
 
-adminSchema.pre('aggregate', function (next) {
-  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } });
-  next();
-});
-
-//checking if user is already exist!
-adminSchema.statics.isUserExists = async function (id: string) {
-  const existingUser = await Admin.findOne({ id });
-  return existingUser;
+// Static method: Check if a user is blocked
+adminSchema.statics.isUserBlocked = async function (userId: string) {
+  const user = await Admin.findOne({ id: userId });
+  return user ? user.isBlocked : false;
 };
 
 export const Admin = model<TAdmin, AdminModel>('Admin', adminSchema);
-
-

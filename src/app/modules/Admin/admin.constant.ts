96ecf4c6ca4dcdbaa77d4c7c95d115969740051d
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-20 12:59:46 +0600

    admin.constant added

--- src/app/modules/Admin/admin.constant.ts
+++ src/app/modules/Admin/admin.constant.ts
@@ -1,28 +1,15 @@
 
 
-import { TBloodGroup, TGender } from './admin.interface';
-
-export const Gender: TGender[] = ['male', 'female', 'other'];
-
-export const BloodGroup: TBloodGroup[] = [
-  'A+',
-  'A-',
-  'B+',
-  'B-',
-  'AB+',
-  'AB-',
-  'O+',
-  'O-',
-];
 
 export const AdminSearchableFields = [
-  'email',
-  'id',
-  'contactNo',
-  'emergencyContactNo',
-  'name.firstName',
-  'name.lastName',
-  'name.middleName',
+  'email',          // Admins may search users by email
+  'name',           // Full name of the user
+  'role',           // Role of the user (admin/user)
+  '_id',            // Unique identifier of the user
 ];
 
+export const AdminActions = {
+  BlockUser: 'BLOCK_USER',   // Admin can block a user
+  DeleteBlog: 'DELETE_BLOG', // Admin can delete any blog
+};
 

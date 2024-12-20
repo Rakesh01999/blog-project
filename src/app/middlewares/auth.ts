a645d48622c872620c6154c82a74dc49f2ce88ec
Rakesh Biswas <rbiswas01999@gmail.com>
2024-12-20 23:35:22 +0600

    index, auth, user.interface modified

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,24 +1,22 @@
-
-
 import { NextFunction, Request, Response } from 'express';
 import httpStatus from 'http-status';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../config';
 import AppError from '../error/AppError';
 import { TUserRole } from '../modules/user/user.interface';
-import { User } from '../modules/user/user.model';
+import { UserModel } from '../modules/user/user.model';
 import catchAsync from '../utils/catchAsync';
 
 const auth = (...requiredRoles: TUserRole[]) => {
   return catchAsync(async (req: Request, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization;
+    const token = req.headers.authorization?.split(' ')[1]; // Extract Bearer token
 
-    // checking if the token is missing
+    // Check if token is missing
     if (!token) {
       throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized!');
     }
 
-    // checking if the given token is valid
+    // Verify the token
     const decoded = jwt.verify(
       token,
       config.jwt_access_secret as string,
@@ -26,48 +24,58 @@ const auth = (...requiredRoles: TUserRole[]) => {
 
     const { role, userId, iat } = decoded;
 
-    // checking if the user is exist
-    const user = await User.isUserExistsByCustomId(userId);
-
-    if (!user) {
-      throw new AppError(httpStatus.NOT_FOUND, 'This user is not found !');
-    }
-    // checking if the user is already deleted
-
-    const isDeleted = user?.isDeleted;
-
-    if (isDeleted) {
-      throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted !');
-    }
-
-    // checking if the user is blocked
-    const userStatus = user?.status;
-
-    if (userStatus === 'blocked') {
-      throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked ! !');
-    }
-
-    if (
-      user.passwordChangedAt &&
-      User.isJWTIssuedBeforePasswordChanged(
-        user.passwordChangedAt,
-        iat as number,
-      )
-    ) {
-      throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized !');
-    }
-
-    if (requiredRoles && !requiredRoles.includes(role)) {
+    // Ensure 'iat' is defined
+    if (!iat) {
       throw new AppError(
         httpStatus.UNAUTHORIZED,
-        'You are not authorized  hi!',
+        'Token issued-at time (iat) is missing or invalid!',
       );
     }
 
-    req.user = decoded as JwtPayload;
+    // Fetch user from the database
+    const user = await UserModel.findById(userId);
+
+    if (!user) {
+      throw new AppError(httpStatus.NOT_FOUND, 'This user does not exist!');
+    }
+
+    // Check if the user is deleted
+    if (user.isDeleted) {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted!');
+    }
+
+    // Check if the user is blocked
+    if (user.isBlocked) {
+      throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked!');
+    }
+
+    // Check if password was changed after JWT issued
+    if (
+      user.passwordChangedAt &&
+      new Date(user.passwordChangedAt).getTime() > iat * 1000
+    ) {
+      throw new AppError(
+        httpStatus.UNAUTHORIZED,
+        'You are not authorized! Token is invalid.',
+      );
+    }
+
+    // Check if the user has the required role
+    if (requiredRoles.length && !requiredRoles.includes(role)) {
+      throw new AppError(
+        httpStatus.FORBIDDEN,
+        'You do not have permission to perform this action!',
+      );
+    }
+
+    // Attach user information to the request object
+    req.user = {
+      id: user._id,
+      role: user.role,
+    };
+
     next();
   });
 };
 
 export default auth;
-
